416202ae7f5d2ccf57983e6df8935c86136f7121
DEEPTHI <deepthisrinivas749@gmail.com>
2026-09-25 11:54:27 +0530

    Update Problem 2 – Longest Stable Signal Window.py

--- Problem 2 – Longest Stable Signal Window.py
+++ Problem 2 – Longest Stable Signal Window.py
@@ -5,15 +5,11 @@ k = int(input())
 left = 0
 best = 0
 start = 1
-
 for right in range(n):
     while max(a[left:right+1]) - min(a[left:right+1]) > k:
         left += 1
-
     length = right - left + 1
-
     if length > best:
         best = length
         start = left + 1
-
 print(best, start)
